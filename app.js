233237a127a03ac186d128f015a969e03d5cc50f
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-09-02 13:06:21 +0300

    fixing read and deploy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require('http').Server(app);
 
 var socketio = require('socket.io')
 
-
+require('dotenv').config();
 
 
 
@@ -31,8 +31,10 @@ app.get('/student', (req, res) => {
     res.render('student')
 })
 
-const server = app.listen(process.env.PORT || 4212, () => {
-    console.log("server is running")
+const port = process.env.PORT || 4121
+
+const server = app.listen(port , () => {
+    console.log(`server is running ${port}`)
 })
 
 
